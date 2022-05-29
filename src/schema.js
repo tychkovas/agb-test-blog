ebf4e228bb838a034cf7a6255a4947a229cd8330
Aleksandr T <tychkovas@yandex.ru>
2022-05-29 18:38:11 +0400

    chore(schema): add commnets

--- src/schema.js
+++ src/schema.js
@@ -8,21 +8,30 @@ const typeDefs = gql`
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "posts" query returns an array of zero or more posts (defined above).
-  "Get posts array"
+  "Get data"
   type Query {
+    "Query to get pusts array"
     posts: [Post]
+    "Fetch a specific post, provided a post's ID"
     post(id: ID!): Post
+    "Query to get users array"
     users: [User]
+    "Fetch a specific user, provided a user's ID"
     user(id: ID!): User
   }
 
+  "Change data"
   type Mutation {
+    "Create new post"
     createPost(content: ContentPublishPost!): ContentPostResponse!
+    "Update specific post, provided a post's ID"
     updatePost(id: ID!, content: ContentPublishPost!): ContentPostResponse!
+    "Delete specific post by ID"
     deletePost(id: ID!): ContentPostResponse! #TODO return id
   }
 
-  interface MutationResponse {
+  "Mutation response common interface"
+    interface MutationResponse {
     "Similar to HTTP status code, represents the status of the mutation"
     code: Int!
     "Indicates whether the mutation was successful"
