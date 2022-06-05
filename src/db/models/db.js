96a20b8ac215e2cb5efa480a7c942123b544b077
Aleksandr T <tychkovas@yandex.ru>
2022-06-06 01:27:43 +0400

    feat(db): add crud post

--- src/db/models/db.js
+++ src/db/models/db.js
@@ -81,4 +81,44 @@ db.getComment = async (request) => {
   return db.Comment.findByPk(id);
 }
 
+db.createPost = async (_post) => {
+  console.log('db:createPost:_post =', _post);
+  const post = _post;
+  if (!post?.published_at) post.published_at = new Date();
+  post.userId = 1;
+  console.log('db:createPost:post = ', post);
+  const newPost = await db.Post.create(post)
+  console.log('db:createPost:newPost.title =', newPost.title);
+  return newPost;
+}
+
+db.updatePost = async (id, _post) => {
+  console.log('db:updatedPost: id, _post =', id, _post);
+  const post = await db.Post.findByPk(id);
+  console.log('db:updatedPost:old post.title =', post.title);
+  if (post) {
+    post.set(_post);
+    if (!post.userId)  post.userId = 1;
+    await post.save();
+  } else {
+    throw new Error('not find post');
+  }
+  console.log(`db:updatePost:new post.title = ${post.title}`);
+  return post;
+}
+
+db.deletePost = async (id) => {
+  console.log('db:deletePost:id:', id);
+  const post = await db.Post.findByPk(id);
+  console.log(`db:deletePost:post.title: ${post.title}`);
+  if (post) {
+    await post.destroy();
+  } else {
+    throw new Error('not find post');
+  }
+  console.log(`db:deletePost:OK:post.title = ${post.title}`);
+
+  return post;
+}
+
 module.exports = db;
