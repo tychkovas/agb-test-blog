37c0be34422cfe07a047d9290d56e58f3a19c512
Aleksandr T <tychkovas@yandex.ru>
2022-06-05 20:45:49 +0400

    chore(db): add log message

--- src/db/models/db.js
+++ src/db/models/db.js
@@ -8,6 +8,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+console.log('__dirname: ', __dirname);
+console.info(`env = ${env}`);
+
 let sequelize;
 
 if (config.use_env_variable) {
