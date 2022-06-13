8ca90573afbfd456b8accba13ae35ebdfd9b4985
Aleksandr T <tychkovas@yandex.ru>
2022-06-13 09:28:00 +0400

    feat(resolver): save query posts

--- src/graphql/resolvers/rPost.js
+++ src/graphql/resolvers/rPost.js
@@ -2,77 +2,144 @@ import Sequelize from 'sequelize';
 import { combineResolvers } from 'graphql-resolvers';
 
 // import pubsub, { EVENTS } from '../subscription';
-import { isAuthenticated, isMessageOwner } from './authorization.js';
-
-const toCursorHash = (string) => Buffer.from(string).toString('base64');
-
-const fromCursorHash = (string) => Buffer.from(string, 'base64').toString('ascii');
+import { isAuthenticated, isPostOwner } from './authorization.js';
 
 export default {
   Query: {
-    posts: async (parent, { cursor, limit = 100 }, { models }) => {
-      const cursorOptions = cursor
-        ? {
-          where: {
-            createdAt: {
-              [Sequelize.Op.lt]: fromCursorHash(cursor),
-            },
-          },
-        }
-        : {};
-
+    // posts: (_, __, { models }) => models.getPosts(),
+    posts: async (_, { page = 1, per_page: perPage = 3 }, { models }) => {
+      console.log('Query:posts: page, per_page:', page, perPage);
+      if (page <= 0) throw new Error('posts: in: error: page <= 0');
       const posts = await models.Post.findAll({
-        order: [['createdAt', 'DESC']],
-        limit: limit + 1,
-        ...cursorOptions,
+        order: [['published_at', 'DESC']],
+        // limit: perPage + 1,
       });
 
-      const hasNextPage = posts.length > limit;
-      const edges = hasNextPage ? posts.slice(0, -1) : posts;
+      const totalPosts = posts.length;
+      const totalPages = Math.ceil(posts.length / perPage);
+
+      const hasNextPage = posts.length > (perPage * page);
+      const curPage = (totalPages > page) ? page : totalPages;
+      const first = perPage * (curPage - 1);
+      const last = hasNextPage ? perPage * curPage : posts.length;
+      const edges = posts.slice(first, last);
+
+      console.log(`Query:posts:totalPosts: ${totalPosts}, totalPages: ${totalPages}, hasNextPage: ${hasNextPage}`);
+      console.log('Query:posts:first: ', first, 'last: ', last);
 
       return {
-        edges,
+        posts: edges,
         pageInfo: {
+          totalPages,
+          totalPosts,
           hasNextPage,
-          endCursor: toCursorHash(
-            edges[edges.length - 1].createdAt.toString(),
-          ),
         },
       };
     },
-    post: async (parent, { id }, { models }) => models.Post.findByPk(id),
+
+    // post: (_, args, { models }) => models.getPost(args),
+
+    post: async (_, { id }, { models }) => {
+      console.log('Query:post:id:', id);
+      return models.Post.findByPk(id);
+    },
   },
 
   Mutation: {
-    createMessage: combineResolvers(
-      isAuthenticated,
-      async (parent, { text }, { models, me }) => {
-        const post = await models.Post.create({
-          text,
-          userId: me.id,
-        });
+    // where our new resolver function will go
+    createPost: async (_, { content }, { db }) => {
+      console.log('Mutation:createPost', content);
+      const newPost = await db.createPost(content);
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully created post '${newPost.title}'`,
+        contentPost: newPost,
+      };
+    },
 
-        // pubsub.publish(EVENTS.MESSAGE.CREATED, {
-        //   messageCreated: { post },
-        // });
+    updatePost: async (_, { id, content }, { db }) => {
+      console.log('Mutation:updatePost:', id, content);
+      try {
+        const updatedPost = await db.updatePost(id, content);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully update post '${updatedPost.title}'`,
+          contentPost: updatedPost,
+        };
+      } catch (e) {
+        console.log('Mutation:updatePost:error:', e.message);
+        return {
+          code: 404,
+          success: false,
+          message: `Error: update post: ${e.message}`,
+          contentPost: null,
+        };
+      }
+    },
 
-        return post;
-      },
-    ),
+    deletePost: async (_, { id }, { db }) => {
+      console.log('Mutation:deletePost:id', id);
+      try {
+        const deletedPost = await db.deletePost(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully delete post '${deletedPost.title}'`,
+          contentPost: deletedPost,
+        };
+      } catch (e) {
+        console.log('Mutation:deletePost:error:', e.message);
+        return {
+          code: 404,
+          success: false,
+          message: `Error: delete post: ${e.message}`,
+          contentPost: null,
+        };
+      }
+    },
 
-    deleteMessage: combineResolvers(
-      isAuthenticated,
-      isMessageOwner,
-      async (parent, { id }, { models }) => models.Post.destroy({ where: { id } }),
-    ),
   },
 
+  // Mutation: {
+  //   createPost: combineResolvers(
+  //     isAuthenticated,
+  //     async (parent, { text }, { models, me }) => {
+  //       const post = await models.Post.create({
+  //         text,
+  //         userId: me.id,
+  //       });
+
+  //       // pubsub.publish(EVENTS.MESSAGE.CREATED, {
+  //       //   postCreated: { post },
+  //       // });
+
+  //       return post;
+  //     },
+  //   ),
+
+  //   deletePost: combineResolvers(
+  //     isAuthenticated,
+  //     isPostOwner,
+  //     async (parent, { id }, { models }) => models.Post.destroy({ where: { id } }),
+  //   ),
+  // },
+
+  // Post: {
+  //   user: async (post, args, { loaders }) => loaders.user.load(post.userId),
+  // },
+
   Post: {
-    user: async (post, args, { loaders }) => loaders.user.load(post.userId),
+    author: async (arg, _, { db }) => {
+      const { userId } = arg;
+      console.info('-resolvers:Post:author:id=', JSON.stringify(arg));
+      return db.getUser({ id: userId });
+    },
   },
 
   // Subscription: {
-  //   messageCreated: {
+  //   postCreated: {
   //     subscribe: () => pubsub.asyncIterator(EVENTS.MESSAGE.CREATED),
   //   },
   // },
