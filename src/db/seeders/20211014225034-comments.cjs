ee411fbae87b7bbd9fdeb9679a68d90b244ec831
Aleksandr T <tychkovas@yandex.ru>
2022-06-13 09:33:54 +0400

    feat(seeder db): change default Data

--- src/db/seeders/20211014225034-comments.cjs
+++ src/db/seeders/20211014225034-comments.cjs
@@ -8,9 +8,9 @@ module.exports = {
         {
           userId: 1,                 
           body: 'comments1: Lorem ipsum, Dolor sit, Consectetuer',
-          published_at: new Date(),
-          createdAt: new Date(),
-          updatedAt: new Date(),
+          published_at: new Date(Date.UTC(2020, 1, 9, 9, 0, 0, 0)),
+          createdAt: new Date(Date.UTC(2020, 1, 9, 9, 0, 0, 0)),
+          updatedAt: new Date(Date.UTC(2021, 2, 9, 9, 0, 0, 0)),
         },
         {
           userId: 2,          
