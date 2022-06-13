6cf3d44756fee0dd12db21c1109af670da70cabd
Aleksandr T <tychkovas@yandex.ru>
2022-06-13 09:30:09 +0400

    feat(schema): save posts
    type User rm  password

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -11,9 +11,9 @@ const typeDefs = gql`
   "Get data"
   type Query {
     "Query to get pusts array"
-    posts: [Post]
+    posts(page: Int, per_page: Int): PagePosts
     "Fetch a specific post, provided a post's ID"
-    post(id: Int!): Post
+    post(id: Int!): PostResponse
     "Query to get users array"
     users: [User]
     "Fetch a specific user, provided a user's ID"
@@ -59,6 +59,23 @@ const typeDefs = gql`
     success: Boolean!
     "Human-readable message for the UI"
     message: String
+  }
+
+  type PagePosts {
+    posts: [PostResponse]
+    pageInfo: PageInfo
+  }
+
+  type PageInfo {
+    totalPages: Int!
+    totalPosts: Int!
+    hasNextPage: Boolean!
+  }
+
+  type PostResponse {
+    title: String!
+    body: String!
+    published_at: DateTime
   }
 
   input ContentPublishPost {
@@ -114,7 +131,7 @@ const typeDefs = gql`
     id: Int!
     nickname: String!
     email: String!
-    password: String!
+    # password: String!
   }
 `;
 
