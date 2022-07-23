ab6c32435610b8cc73316470e3bce622b4598f18
Aleksandr T <tychkovas@yandex.ru>
2022-07-23 10:20:06 +0400

    feat(avatar edit): show in row, change properties - sketch

--- src/AvatarEditor.jsx
+++ src/AvatarEditor.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 // https://kirill3333.github.io/react-avatar/
 import Avatar from 'react-avatar-edit'
 
+const SOURCE_PATH = '';
+
 class AvatarEditor extends React.Component {
 
   constructor(props) {
@@ -15,6 +17,7 @@ class AvatarEditor extends React.Component {
     this.onCrop = this.onCrop.bind(this)
     this.onClose = this.onClose.bind(this)
     this.onBeforeFileLoad = this.onBeforeFileLoad.bind(this)
+    this.onLoadNewImage = this.onLoadNewImage.bind(this)
   }
  
   onClose() {
@@ -31,19 +34,43 @@ class AvatarEditor extends React.Component {
       elem.target.value = "";
     };
   }
+
+  onLoadNewImage() {
+    const src = './einshtein.jpg'
+    this.setState({src})
+    console.log('src =', src);
+  }
   
   render () {
     return (
-      <div>
-        <Avatar
-          width={300}
-          height={300}
-          onCrop={this.onCrop}
-          onClose={this.onClose}
-          onBeforeFileLoad={this.onBeforeFileLoad}
-          src={this.state.src}
-        />
-        <img src={this.state.preview} alt="Preview" />
+      <div className="container-fluid" >
+        <div className="row">
+          <div className="col-2"/>
+          <div className="col-5">
+            <Avatar
+              width={300}
+              // height={400}
+              imageWidth={300}
+              cropRadius={150}
+              onCrop={this.onCrop}
+              onClose={this.onClose}
+              onBeforeFileLoad={this.onBeforeFileLoad}
+              src={this.state.src}
+              // img={'./einshtein.jpg'}
+              exportAsSquare
+              exportSize={300}
+            />
+            <div  style={{paddingTop: 20}}>
+              <button onClick={this.onLoadNewImage} type="button" className="btn btn-primary">Load another image</button>
+            </div>
+          </div>
+          <div className="col-2"/>
+          <div className="col-3 ">
+            <h5>Preview</h5>
+            <img alt="" style={{width: '200px', height: '200px'}} src={this.state.preview}/>
+          </div>
+        </div>
+        {/* <img src={this.state.preview} alt="Preview" /> */}
       </div>
     )
   }
