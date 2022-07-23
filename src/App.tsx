fd07289c182cd6a249b8572cff3f4d5859706d54
Aleksandr T <tychkovas@yandex.ru>
2022-07-23 10:52:32 +0400

    refactor(app): import bootstrap, function to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 import Avatar from 'react-avatar';
 import AvatarEditor from './AvatarEditor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Avatar name="App logo" src={logo} />
-        <Avatar name="My Avatar" size="100" round="20px"/>
-        <br />
-        <h3>Load Avatar</h3>
-        <div>
-          <AvatarEditor />
-        </div>
-      </header>
-    </div>
-  );
+type Props = {};
+
+type State = {};
+
+// function App() {
+class App extends Component<Props, State> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Avatar name="App logo" src={logo} />
+          <Avatar name="My Avatar" size="100" round="20px"/>
+          <br />
+          <h3>Load Avatar</h3>
+          <div>
+            <AvatarEditor />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
