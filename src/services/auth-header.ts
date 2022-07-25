378dc57490596bbd38862968899762f676f47225
Aleksandr T <tychkovas@yandex.ru>
2022-07-25 13:32:16 +0400

    fix(auth): connect server error taken cannot be null

--- src/services/auth-header.ts
+++ src/services/auth-header.ts
@@ -1,12 +1,10 @@
 export default function authHeader() {
+  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("token");
-
-
 
   console.log(`localStorage.getItem token =  ${token}.`);
 
   // return { Authorization: 'Bearer ' + token }; // for Spring Boot back-end
   // return { 'x-access-token': token };       // for Node.js Express back-end
-  return { 'x-token': token };
-
+  return { 'x-token': token ? token : '' };
 }
