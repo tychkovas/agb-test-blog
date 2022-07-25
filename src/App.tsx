be7f10b2ec431369234c0b262feeec35f9a31c42
Aleksandr T <tychkovas@yandex.ru>
2022-07-25 12:50:35 +0400

    feat(app): add /login /register  page add state currentUser

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import { Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import IUser from './types/user.type';
+
 import DisplayUsers from './components/displayusers.component'
 import Home from './components/home.component'
+import Register from './components/register.component';
+import Login from './components/login.component'
 
 type Props = {};
 
-type State = {};
+type State = {
+  currentUser: IUser | undefined
+};
 
 class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      currentUser: undefined,
+    };
+  }
   render() {
+    const { currentUser } = this.state;
+
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -30,11 +45,30 @@ class App extends Component<Props, State> {
               </Link>
             </li>
           </div>
+
+          {(
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+
         </nav>
         <div className="container mt-3">
           <Switch>
             <Route exact path={["/", "/home"]} component={Home} />
             <Route path="/users" component={DisplayUsers} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
