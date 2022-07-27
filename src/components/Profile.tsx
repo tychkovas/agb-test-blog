3825c56236bd286c135ca4c5f3cc987d0bf554ca
Aleksandr T <tychkovas@yandex.ru>
2022-07-27 17:03:57 +0400

    refactor(component): profile: add avatar editor add avatar (sketch)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { getCurrentUser } from "../services/auth.service";
+import Avatar from 'react-avatar';
+import AvatarEditor from '../utils/AvatarEditor';
 
 const Profile: React.FC = () => {
   const currentUser = getCurrentUser();
   
   return (
-    <div className="container">
+    <div className="container">  
       <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <h3>
+                <strong>{currentUser.username}</strong> Profile
+              </h3>
+            </div>
+            <div className="col-xs">
+              <Avatar name="My Avatar" size="100" round="20px"/>
+            </div>
+          </div>
+        </div>
       </header>
       <p>
         <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
@@ -26,6 +38,15 @@ const Profile: React.FC = () => {
         {currentUser.roles &&
           currentUser.roles.map((role: string, index: number) => <li key={index}>{role}</li>)}
       </ul>
+
+      <hr/>
+      <h3>Load Avatar</h3>
+      <div className="container bg-secondary" >
+        <AvatarEditor />
+      </div>
+      <hr/>
+      <br/>
+
     </div>
   );
 };
