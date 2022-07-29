d702495566ca18ce0b81aee1da1cb8574533d21c
Aleksandr T <tychkovas@yandex.ru>
2022-07-29 17:33:24 +0400

    feat(s3): Implementing the upload logic

--- src/s3/AWSS3Uploader.js
+++ src/s3/AWSS3Uploader.js
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import stream from 'stream';
 
 export class AWSS3Uploader {
   constructor(config) {
@@ -18,22 +19,50 @@ export class AWSS3Uploader {
     this.config = config;
   }
 
-  async singleFileUploadResolver ( parent, { file }, ) {
-    const { stream, filename, mimetype, encoding } = await file;
+  createDestinationFilePath = (fileName) => fileName;
+
+  createUploadStream = (key) => {
+    const pass = new stream.PassThrough();
+    return {
+      writeStream: pass,
+      promise: this.s3
+        .upload({
+          Bucket: this.config.destinationBucketName,
+          Key: key,
+          Body: pass,
+        })
+        .promise(),
+    };
+  };
+
+  async singleFileUploadResolver(_parent, { file }, _context) {
+    const {
+      stream: fileStream, filename, mimetype, encoding,
+    } = await file;
 
     // Create the destination file path
     const filePath = this.createDestinationFilePath(
       filename,
       mimetype,
-      encoding
+      encoding,
     );
 
     // Create an upload stream that goes to S3
+    const uploadStream = this.createUploadStream(filePath);
+
     // Pipe the file data into the upload stream
+    fileStream.pipe(uploadStream.writeStream);
+
+    // Start the stream
+    const result = await uploadStream.promise;
+
     // Get the link representing the uploaded file
+    // const link = result.Location;
     // (optional) save it to our database
 
-    return { filename, mimetype, encoding, url: '' };
+    return {
+      filename, mimetype, encoding, url: result.Location,
+    };
   }
 }
 
