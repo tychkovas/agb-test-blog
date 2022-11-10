e0a3bff93dfe3a87764854cff19fc424376adc65
tychkovas <tychkovas@yandex.ru>
2022-11-10 12:30:22 +0400

    feat(types): update IUser

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,7 +1,9 @@
 export default interface IUser {
   id?: any | null,
-  username: string,
+  username?: string,
+  nickname?: string | null,
   email: string,
   password: string,
+  avatar_src?: string | null,
   roles?: Array<string>
 }
